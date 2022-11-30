11fe6438995389e92fbd5d8fd8b68fe2edb39a43
gianf <federicovittoriolucio.dellascenza@studentmail.unicas.it>
2022-11-30 15:07:53 +0100

    Sono state eseguite alcune prove fallimentari.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module it.unicas.engsoftwareproject {
 
     opens it.unicas.engsoftwareproject to javafx.fxml;
     exports it.unicas.engsoftwareproject;
+    exports it.unicas.engsoftwareproject.controller;
+    opens it.unicas.engsoftwareproject.controller to javafx.fxml;
 }
